087abd5b26223c3b9bffa8185aed2c0a349966c1
hendrix <hendrixperalta@gmail.com>
2018-06-16 01:44:12 -0400

    Initial commit

---
+++ app/src/main/java/com/hdx/newvoices/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.hdx.newvoices;
+
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<android.support.v4.app.Fragment> lstFragment = new ArrayList<>();
+    private final List<String> lstTitles = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public android.support.v4.app.Fragment getItem(int position) {
+        return lstFragment.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return lstTitles.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return lstTitles.get(position);
+    }
+
+    public void addFragment (android.support.v4.app.Fragment fragment, String string){
+        lstTitles.add(string);
+        lstFragment.add(fragment);
+
+    }
+}
